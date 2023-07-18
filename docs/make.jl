ce457794b915c34a2f423f08d244137b4b5e3355
Sven Duve <svenduve@gmail.com>
2023-07-18 21:59:16 +0200

    add RLTypes in make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,16 @@
 using Rewards
 using Documenter
 
+
+try
+    using RLTypes
+catch
+    using Pkg
+    Pkg.add(url="https://github.com/SvenDuve/RLTypes.jl.git")
+    println("RLTypes.jl was not installed, but is now.")
+    using RLTypes
+end
+
 DocMeta.setdocmeta!(Rewards, :DocTestSetup, :(using Rewards); recursive=true)
 
 makedocs(;
