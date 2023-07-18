4eb9fabc8baa6532e6e39965919b751e895a4679
Sven Duve <svenduve@gmail.com>
2023-07-18 22:01:33 +0200

    added loadpath

--- docs/make.jl
+++ docs/make.jl
@@ -1,15 +1,7 @@
+push!(LOAD_PATH,"../src/")
 using Rewards
 using Documenter
 
-
-try
-    using RLTypes
-catch
-    using Pkg
-    Pkg.add(url="https://github.com/SvenDuve/RLTypes.jl.git")
-    println("RLTypes.jl was not installed, but is now.")
-    using RLTypes
-end
 
 DocMeta.setdocmeta!(Rewards, :DocTestSetup, :(using Rewards); recursive=true)
 
